620222b1adab2539fc24b894c538b85e001d42b3
Paige Hetherington <herbmama@gmail.com>
2016-03-28 11:16:42 -0400

    add stuff

--- src/cards_clojure/core.clj
+++ src/cards_clojure/core.clj
@@ -1,5 +1,32 @@
 (ns cards-clojure.core
   (:gen-class))
 
+(def suits [:clubs :spades :hearts :diamonds])
+(def ranks (range 1 14)) ; first inclusive/last exclusive so will be 1-13
+(def rank-names {1 :ace, 11 :jack, 12 :queen, 13 :king})
+
+(defn create-deck []
+  (set ;returns list and turns into set
+    (for [suit suits]
+        rank ranks)
+    {:suit suit
+      :rank (get rank-names rank rank)})) ; 3rd argument to get as default
+
+(defn create-hands [deck]
+  (set
+    (for [c1 deck
+          c2 (disj deck c1)
+          c3 (disj deck c1 c2)
+          c4 (disj deck c1 c2 c3)]
+      #{c1 c2 c3 c4}))) ; HS 
+
+(defn flush? [hand]
+  (=1 count (set (map :suit hand))))
+
 (defn -main []
-  (println "Hello, World!"))
+  (time ;benchmark
+    (let [deck (create-deck)
+          hands (create-hands deck)
+          flush-hands (filter flush? hands)]
+      (count flush-hands))))
+            
